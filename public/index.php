2a9d58238e56912d66a972b08a6df4190f13455f
nht0017 <nht0017@users.noreply.github.com>
2019-10-22 19:23:47 -0700

    Added login file
    Added login link to homepage

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ $page_title = "DP Portal";
 include('header.php'); 
 include('config.php');
 ?>
-
+<h1>Welcome to Doctor Patient Portal</h1>
 <p>This is the main text for the home page of the doctor patient portal!</p>
-
+<a href="../login/login.html">Login here!</a><br>
 <?php include('footer.php'); ?>
